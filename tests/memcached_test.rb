7cc1cc854f64299d4a24edad0520c4cf56c399e0
Nathaly Sotomayor <nathysoto12.07@gmail.com>
2020-07-04 13:58:08 -0500

    Update tests

--- tests/memcached_test.rb
+++ tests/memcached_test.rb
@@ -1,35 +1,40 @@
 require File.expand_path('../../lib/memcached', __FILE__)
-require 'test/unit'
+require 'minitest/autorun'
 
-class MemcachedTest < Test::Unit::TestCase
+class TestMemcached < Minitest::Test
   def setup
+    # Create instances to use in test cases
     @memcached = Memcached.new
     @set_example = { command: 'set', key: 'key', flags: '0', ttl: 10, value: 'value' }
     @other_set_example = { command: 'set', key: 'otherkey', flags: '1', ttl: 10, value: 'othervalue' }
     @get_example = { command: 'get', keys: ['key'] }
   end
 
-  def test_setting_record_returns_stored
+  def test_set_registry
+    # Successful case (STORED)
     res = @memcached.process(@set_example)
     assert_equal("STORED\r\n\n", res)
     res = @memcached.process(@get_example)
     assert_equal("VALUE key 0 \r\nvalue\r\nEND\r\n\n", res)
   end
 
-  def test_adding_existing_record_returns_not_stored
+  def test_add_existing_registry
+    # Not successful case (NOT STORED)
     @memcached.process(@set_example)
     add_args = { command: 'add', key: 'key', flags: '0', ttl: 1, value: 'value' }
     res = @memcached.process(add_args)
     assert_equal("NOT_STORED\r\n\n", res)
   end
 
-  def test_adding_unexisting_record_returns_stored
+  def test_add_unexisting_registry
+    # Successful case (STORED)
     add_args = { command: 'add', key: 'key', flags: '0', ttl: 1, value: 'value' }
     res = @memcached.process(add_args)
     assert_equal("STORED\r\n\n", res)
   end
 
-  def test_appending_existing_record_returns_stored
+  def test_append_existing_registry
+    # Successful case (STORED)
     @memcached.process(@set_example)
     append_args = { command: 'append', key: 'key', flags: '0', ttl: 1, value: 'other' }
     res = @memcached.process(append_args)
@@ -38,27 +43,15 @@ class MemcachedTest < Test::Unit::TestCase
     assert_equal("VALUE key 0 \r\nvalueother\r\nEND\r\n\n", res)
   end
 
-  def test_appending_unexisting_record_returns_not_stored
+  def test_append_unexisting_registry
+    # Not successful case (NOT STORED)
     append_args = { command: 'replace', key: 'key', flags: '0', ttl: 1, value: 'value' }
     res = @memcached.process(append_args)
     assert_equal("NOT_STORED\r\n\n", res)
   end
 
-  def test_replacing_existing_record_returns_stored
-    @memcached.process(@set_example)
-    replace_args = { command: 'replace', key: 'key', flags: '0', ttl: 1, value: 'value' }
-    res = @memcached.process(replace_args)
-    assert_equal("STORED\r\n\n", res)
-  end
-
-  def test_replacing_unexisting_record_returns_not_stored
-    replace_args = { command: 'replace', key: 'key', flags: '0', ttl: 1, value: 'value' }
-    res = @memcached.process(replace_args)
-    assert_equal("NOT_STORED\r\n\n", res)
-  end
-
-
-  def test_prepending_existing_record_returns_stored
+  def test_prepend_existing_registry
+    # Successful case (STORED)
     @memcached.process(@set_example)
     prepend_args = { command: 'prepend', key: 'key', flags: '0', ttl: 1, value: 'other' }
     res = @memcached.process(prepend_args)
@@ -67,13 +60,30 @@ class MemcachedTest < Test::Unit::TestCase
     assert_equal("VALUE key 0 \r\nothervalue\r\nEND\r\n\n", res)
   end
 
-  def test_prepending_unexisting_record_returns_not_stored
+  def test_prepend_unexisting_registry
+    # Not successful case (NOT STORED)
     prepend_args = { command: 'replace', key: 'key', flags: '0', ttl: 1, value: 'value' }
     res = @memcached.process(prepend_args)
     assert_equal("NOT_STORED\r\n\n", res)
   end
 
-  def test_casing_unmodified_record_returns_stored
+  def test_replace_existing_registry
+    # Successful case (STORED)
+    @memcached.process(@set_example)
+    replace_args = { command: 'replace', key: 'key', flags: '0', ttl: 1, value: 'value' }
+    res = @memcached.process(replace_args)
+    assert_equal("STORED\r\n\n", res)
+  end
+
+  def test_replace_unexisting_registry
+    # Not successful case (NOT STORED)
+    replace_args = { command: 'replace', key: 'key', flags: '0', ttl: 1, value: 'value' }
+    res = @memcached.process(replace_args)
+    assert_equal("NOT_STORED\r\n\n", res)
+  end
+
+  def test_cas_unmodified_registry
+    # Successful case (STORED)
     @memcached.process(@set_example)
     object_id = @memcached.storage[@set_example[:key]].object_id
     cas_args = {
@@ -89,7 +99,8 @@ class MemcachedTest < Test::Unit::TestCase
     assert_equal("STORED\r\n\n", res)
   end
 
-  def test_casing_modified_record_returns_exists
+  def test_cas_modified_registry
+    # Not successful case (EXISTS)
     @memcached.process(@set_example)
 
     time_before_updating = Time.now
@@ -110,7 +121,8 @@ class MemcachedTest < Test::Unit::TestCase
     assert_equal("EXISTS\r\n\n", res)
   end
 
-  def test_casing_record_without_unique_token_returns_error
+  def test_cas_registry_without_unique_token
+    # Error case (ERROR)
     cas_args = {
       command: 'cas',
       key: 'key',
@@ -123,7 +135,8 @@ class MemcachedTest < Test::Unit::TestCase
     assert_equal("ERROR\r\n\n", res)
   end
 
-  def test_sending_wrong_parameters_returns_error
+  def test_send_wrong_parameters
+    # Error case (ERROR)
     wrong_args = {
       command: 'wrong_command',
       key: 'errorkey',
@@ -135,18 +148,21 @@ class MemcachedTest < Test::Unit::TestCase
     assert_equal("ERROR\r\n\n", res)
   end
 
-  def test_getting_existing_record_returns_value
+  def test_get_existing_registry
+    # Retrieval successful case
     @memcached.process(@set_example)
     res = @memcached.process(@get_example)
     assert_equal("VALUE key 0 \r\nvalue\r\nEND\r\n\n", res)
   end
 
-  def test_getting_unexisting_record_returns_not_found
+  def test_get_unexisting_registry
+    # Retrieval unsuccessful case (not found - unexisting key)
     res = @memcached.process(@get_example)
     assert_equal("NOT_FOUND\r\n\n", res)
   end
 
-  def test_getting_multiple_keys_returns_values
+  def test_get_multiple_keys
+    # Retrieval successful case
     @memcached.process(@set_example)
     @memcached.process(@other_set_example)
     gets_args = { command: 'get', keys: ['key', 'otherkey'] }
@@ -154,7 +170,8 @@ class MemcachedTest < Test::Unit::TestCase
     assert_equal("VALUE key 0 \r\nvalue\r\nVALUE otherkey 1 \r\nothervalue\r\nEND\r\n\n", res)
   end
 
-  def test_getting_cas_record_returns_values_with_unique_token
+  def test_get_cas_registry
+    # returns_values_with_unique_token
     @memcached.process(@set_example)
     object_id = @memcached.storage[@set_example[:key]].object_id
     gets_args = { command: 'gets', keys: ['key'] }
